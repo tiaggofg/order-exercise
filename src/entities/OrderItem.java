56816a739e46b29b5f663cc8386b9afd4dfefe6c
Tiago Godoy <tiago.fernandes.godoy@gmail.com>
2022-05-29 20:03:51 -0300

    Initial commit

---
+++ src/entities/OrderItem.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class OrderItem {
+	
+	private int quantity;
+	private double price;
+	
+	public OrderItem() {
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public double subTotal() {
+		return quantity * price;	
+	}
+
+}
